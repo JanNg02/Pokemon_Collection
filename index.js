7840274826d12200e470273bedbb7eab1fa37199
JanNg02 <114426292+JanNg02@users.noreply.github.com>
2025-10-15 14:46:50 +0800

    mongoose connection made and .gitignore made

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config(); 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const router  = require('./routes.js');
@@ -15,6 +17,17 @@ app.use(express.static(__dirname));
 //Ejs enabled   
 app.set('view engine', 'ejs');
 app.set('View', 'view'); 
+
+//Mongoose
+const mongoAtlasUri = process.env.MONGODB_URI;
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(mongoAtlasUri);
+    console.log("Mongoose is connected")
+
+} catch (e) {
+    console.log("could not connect");
+}
 
 // Start the server
 app.listen(port, () => {
