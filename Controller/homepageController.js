a069adb39a299624f7969e013140703ef515db24
JanNg02 <114426292+JanNg02@users.noreply.github.com>
2025-10-16 18:09:08 +0800

    made the navbar modular and cleaned out comments from homepagecontroller

--- Controller/homepageController.js
+++ Controller/homepageController.js
@@ -7,18 +7,6 @@ const homepageController = {
         console.log("Session Username " + req.session.userName);
 
         try {
-            // // Fetch cards
-            // const cardsResponse = await axios.get('https://api.pokemontcg.io/v2/cards', {
-            //     params: {
-            //         q: 'name:charizard',
-            //         pageSize: 1,
-            //         page: 1
-            //     },
-            //     headers: {
-            //         'X-Api-Key': process.env.POKEMONAPI
-            //     }
-            // });
-
             // Fetch the 3 latest sets
             const setsResponse = await axios.get('https://api.pokemontcg.io/v2/sets', {
                 params: {
@@ -30,16 +18,12 @@ const homepageController = {
                     'X-Api-Key': process.env.POKEMONAPI
                 }
             });
-
-            // console.log(cardsResponse.data);
             console.log(setsResponse.data);
 
             // Ensure responses have data
-            //const cards = cardsResponse.data && cardsResponse.data.data ? cardsResponse.data.data : [];
             const sets = setsResponse.data && setsResponse.data.data ? setsResponse.data.data : [];
 
             // Pass both cards and sets to the homepage view
-            //res.render('homepage', { cards: cards, sets: sets });
             res.render('homepage', {sets: sets});
         } catch (error) {
             console.error('Error fetching Pokemon data:', error);
